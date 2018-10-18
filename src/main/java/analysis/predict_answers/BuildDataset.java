35db5d6bf68c7d0f11984819589405bd648379d8
ehallmark <ehallmark@gttgrp.com>
2018-10-17 21:22:18 -0700

    c

--- src/main/java/analysis/predict_answers/BuildDataset.java
+++ src/main/java/analysis/predict_answers/BuildDataset.java
@@ -1,7 +1,11 @@
 package analysis.predict_answers;
 
+import com.opencsv.CSVWriter;
 import org.eclipse.jetty.util.ArrayUtil;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.sql.*;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -11,10 +15,7 @@ public class BuildDataset {
 
     private static Object[] getFeaturesFor(ResultSet rs, int startIdx) throws SQLException {
         String body = rs.getString(startIdx+1);
-        String tagsStr = rs.getString(startIdx+2);
-        String[] tags = tagsStr==null||tagsStr==null?new String[]{} : Stream.of(tagsStr.split("><"))
-                .map(str->str.replace("<","").replace(">","")).toArray(size->new String[size]);
-        tags = ArrayUtil.removeFromArray(tags, "");
+        String tags = rs.getString(startIdx+2);
         String title = rs.getString(startIdx+3);
         return new Object[]{
                 body,
@@ -25,48 +26,64 @@ public class BuildDataset {
 
     public static void main(String[] args) throws Exception {
         final Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/stackoverflow?user=postgres&password=password&tcpKeepAlive=true");
+        final Connection conn2 = DriverManager.getConnection("jdbc:postgresql://localhost/stackoverflow?user=postgres&password=password&tcpKeepAlive=true");
         conn.setAutoCommit(false);
+        conn2.setAutoCommit(false);
         // we want to match up questions with:
         //      An answer to the question with a positive score with probability 0.5
         //      A random answer of a random question with probability 0.5
 
         // start by getting ids
-        PreparedStatement ps = conn.prepareStatement("select parent_body, parent_tags, parent_title, body, tags, title from posts order by id");
-        PreparedStatement ps2 = conn.prepareStatement("select body, tags, title from answers_with_question order by random()");
+        System.out.println("Starting to read data...");
+        CSVWriter writer = new CSVWriter(new BufferedWriter(new FileWriter(new File("answer_prediction_data.csv"))));
+        PreparedStatement ps = conn.prepareStatement("select coalesce(parent_body,''), coalesce(parent_tags,''), coalesce(parent_title,''), coalesce(body,''), coalesce(tags,''), coalesce(title,'') from answers_with_question order by id");
+        PreparedStatement ps2 = conn2.prepareStatement("select coalesce(body,''), coalesce(tags,''), coalesce(title,'') from answers_with_question order by random()");
         ps.setFetchSize(100);
         ps2.setFetchSize(100);
         ResultSet rs = ps.executeQuery();
         ResultSet rs2 = ps.executeQuery();
-        final List<Object> features = new ArrayList<>();
-        final List<Object> labels = new ArrayList<>();
         int count = 0;
+        System.out.println("Iterating...");
         while(rs.next() && rs2.next()) {
             final Object[] questionFeatures = getFeaturesFor(rs, 0);
             final Object[] actualAnswerFeatures = getFeaturesFor(rs, 4);
             final Object[] randomAnswerFeatures = getFeaturesFor(rs2, 0);
 
-            // actual
-            labels.add(1);
-            features.add(new Object[]{questionFeatures, actualAnswerFeatures});
+            final String[] featuresPos = new String[]{
+                    questionFeatures[0].toString(),
+                    questionFeatures[1].toString(),
+                    questionFeatures[2].toString(),
+                    actualAnswerFeatures[0].toString(),
+                    "1",
+            };
 
-            // random
-            labels.add(0);
-            features.add(new Object[]{questionFeatures, randomAnswerFeatures});
+            writer.writeNext(featuresPos);
 
+            final String[] featuresNeg = new String[]{
+                    questionFeatures[0].toString(),
+                    questionFeatures[1].toString(),
+                    questionFeatures[2].toString(),
+                    randomAnswerFeatures[0].toString(),
+                    "0"
+            };
+
+            writer.writeNext(featuresNeg);
             if(count%1000==999) {
                 System.out.println("Seen answers: " + count);
+                writer.flush();
             }
             count++;
         }
+        writer.flush();
+        writer.close();
         rs.close();
         ps.close();
         rs2.close();
         ps2.close();
 
-        System.out.println("Features size: "+features.size());
-        System.out.println("Labels size: "+labels.size());
-
-
         conn.close();
+        conn2.close();
+
+        System.out.println("Finished.");
     }
 }
