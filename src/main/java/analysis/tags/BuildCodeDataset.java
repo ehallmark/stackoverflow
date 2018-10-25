cfbf648fa2db4ec6612fdcc534e01eb512801ec2
ehallmark <ehallmark@gttgrp.com>
2018-10-25 09:06:53 -0700

    c

--- src/main/java/analysis/tags/BuildCodeDataset.java
+++ src/main/java/analysis/tags/BuildCodeDataset.java
@@ -18,11 +18,14 @@ import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 public class BuildCodeDataset {
-    public static final String DATA_FILE = "/media/ehallmark/tank/stack_tag_code_prediction_data.csv";
+    public static final String CLASSIFICATION_DATA_FILE = "/media/ehallmark/tank/stack_tag_code_prediction_data.csv";
+    public static final String SIMILARITY_DATA_FILE = "/media/ehallmark/tank/stack_tag_code_sim_prediction_data.csv";
 
 
     public static void main(String[] args) throws Exception {
         boolean test = false;
+        final boolean buildDatasetForClassification = false;
+        final boolean buildDatasetForSimilarity = true;
         List<String> codeVocabulary = CSVHelper.readFromCSV("code_vocabulary.csv").stream().map(s->s[0]).collect(Collectors.toList());
         List<String> questionVocabulary = CSVHelper.readFromCSV("answers_vocabulary.csv").stream().map(s->s[0]).collect(Collectors.toList());
         Set<String> tags = CSVHelper.readFromCSV("tags5000.csv").stream().map(s->s[0]).collect(Collectors.toSet());
@@ -31,7 +34,10 @@ public class BuildCodeDataset {
         Map<String,Integer> answerVocabIndexMap = IntStream.range(0, questionVocabulary.size()).mapToObj(i->new Pair<>(questionVocabulary.get(i), i))
                 .collect(Collectors.toMap(e->e.getKey(), e->e.getValue()));
 
-        List<Integer> posts = new LinkedList<>();
+        List<Integer> posts = null;
+        if(buildDatasetForClassification) {
+            posts = new LinkedList<>();
+        }
 
         final Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/stackoverflow?user=postgres&password=password&tcpKeepAlive=true");
         conn.setAutoCommit(false);
@@ -41,8 +47,15 @@ public class BuildCodeDataset {
 
         // start by getting ids
         System.out.println("Starting to read data...");
-        CSVWriter writer = new CSVWriter(new BufferedWriter(new FileWriter(new File(DATA_FILE + (test ? ".test.csv" : "")))));
-        PreparedStatement ps = conn.prepareStatement("select coalesce(body,''), coalesce(tags,''), id, coalesce(parent_id,0), coalesce(accepted_answer_id,0) from posts where parent_id is null");
+        CSVWriter writer1 = null;
+        if(buildDatasetForClassification) {
+            writer1 = new CSVWriter(new BufferedWriter(new FileWriter(new File(CLASSIFICATION_DATA_FILE + (test ? ".test.csv" : "")))));
+        }
+        CSVWriter writer2 = null;
+        if(buildDatasetForSimilarity) {
+            writer2 = new CSVWriter(new BufferedWriter(new FileWriter(new File(SIMILARITY_DATA_FILE + (test ? ".test.csv" : "")))));
+        }
+        PreparedStatement ps = conn.prepareStatement("select coalesce(body,''), coalesce(tags,''), coalesce(parent_id, id), coalesce(parent_id,0), coalesce(accepted_answer_id,0) from posts");
         ps.setFetchSize(100);
         ResultSet rs = ps.executeQuery();
         int count = 0;
@@ -50,35 +63,56 @@ public class BuildCodeDataset {
         int valid = 0;
         final PostsPreprocessor postsPreprocessor = new PostsPreprocessor();
         while(rs.next()) {
-            final String[] questionFeatures = postsPreprocessor.getAllFeaturesFor(rs, 0, answerVocabIndexMap, codeVocabIndexMap, tags);
-            if(questionFeatures!=null && questionFeatures[4].trim().length()>0) {
-                writer.writeNext(questionFeatures);
-                posts.add(rs.getInt(3));
-                valid++;
+            if(buildDatasetForClassification) {
+                if(rs.getInt(4)==0) { // don't want answers (only want questions)
+                    final String[] questionFeatures = postsPreprocessor.getAllFeaturesFor(rs, 0, answerVocabIndexMap, codeVocabIndexMap, tags);
+                    if (questionFeatures != null && questionFeatures[4].trim().length() > 0) {
+                        writer1.writeNext(questionFeatures);
+                        posts.add(rs.getInt(3));
+                        valid++;
+                    }
+                }
+            }
+            if(buildDatasetForSimilarity) {
+                final String[] questionFeatures = postsPreprocessor.getAllFeaturesFor(rs, 0, answerVocabIndexMap, codeVocabIndexMap, tags);
+                if (questionFeatures != null) {
+                    writer2.writeNext(questionFeatures);
+                    valid++;
+                }
             }
 
-            if(count%1000==999) {
-                System.out.println("Seen answers: " + count+". Valid: "+valid);
-                writer.flush();
-                if(test && count > 100000) {
-                    writer.close();
-                    rs.close();
-                    ps.close();
-                    conn.close();
-                    System.exit(0);
+            if (count % 1000 == 999) {
+                System.out.println("Seen answers: " + count + ". Valid: " + valid);
+                if(writer1 != null) {
+                    writer1.flush();
+                }
+                if(writer2 != null) {
+                    writer2.flush();
+                }
+                if (test && count > 100000) {
+                    break;
                 }
             }
             count++;
         }
-        writer.flush();
-        writer.close();
+        if(writer1 != null) {
+            writer1.flush();
+            writer1.close();
+        }
+        if(writer2 != null) {
+            writer2.flush();
+            writer2.close();
+        }
+
         rs.close();
         ps.close();
-
         conn.close();
-        System.out.println("Sorting post ids... (n="+posts.size()+")");
-        Collections.sort(posts);
-        CSVHelper.writeToCSV("all_post_ids.csv", posts.stream().map(id->new String[]{String.valueOf(id)}).collect(Collectors.toList()));
+
+        if(buildDatasetForClassification) {
+            System.out.println("Sorting post ids... (n="+posts.size()+")");
+            Collections.sort(posts);
+            CSVHelper.writeToCSV("all_post_ids.csv", posts.stream().map(id -> new String[]{String.valueOf(id)}).collect(Collectors.toList()));
+        }
         System.out.println("Finished.");
     }
 }
